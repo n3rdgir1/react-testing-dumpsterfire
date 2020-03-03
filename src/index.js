be5833c0dce31506e604b7dd506b7b8ef3d700f7
n3rdgir1 <nerdgirl11235813@gmail.com>
2020-03-02 20:41:57 -0600

    master - niceties, like linting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './rdx/reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducer, composeEnhancers());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
